a62b271c9f47f08a1ae6b69b209f2393a0098caa
Beyza Nur <beyzanurelitok21@gmail.com>
2026-03-31 10:07:18 +0300

    chore: add Unity .gitignore and LFS config

---
+++ Assets/Scripts/FlightController.cs
@@ -0,0 +1,53 @@
+// FlightController.cs
+// CENG 454 – HW1: Sky-High Prototype
+// Author: Beyza Nur Elitok | Student ID: 210444043
+
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = 45f;
+    [SerializeField] private float yawSpeed = 45f;
+    [SerializeField] private float rollSpeed = 45f;
+    [SerializeField] private float thrustSpeed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        HandleRotation();
+        HandleThrust();
+    }
+
+    private void HandleRotation()
+    {
+        // Pitch – Arrow Up/Down
+        float pitch = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right, pitch * pitchSpeed * Time.deltaTime);
+
+        // Yaw – Arrow Left/Right
+        float yaw = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, yaw * yawSpeed * Time.deltaTime);
+
+        // Roll – Q / E
+        float roll = 0f;
+        if (Input.GetKey(KeyCode.Q)) roll = 1f;
+        if (Input.GetKey(KeyCode.E)) roll = -1f;
+        transform.Rotate(Vector3.forward, roll * rollSpeed * Time.deltaTime);
+    }
+
+    private void HandleThrust()
+    {
+        // Spacebar – forward thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime);
+        }
+    }
+}
